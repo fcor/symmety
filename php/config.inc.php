52d49ebb9a213b2c6b8b8774b6bcae03f076a420
Fabio Cortés <fcor14@hotmail.com>
2018-06-19 12:37:54 -0500

    Base files

---
+++ php/config.inc.php
@@ -0,0 +1,14 @@
+<?php
+// SMTP Server Settings
+$config['smtp_host'] 			= 'smtp.gmail.com';   // eg.: smtp.mandrillapp.com
+$config['smtp_port'] 			= 465;						// eg.: 587
+$config['smtp_user'] 			= 'soporte@support-domain342.info'; 						// you@gmail.com
+$config['smtp_pass'] 			= 'Google2016';						// password
+$config['smtp_ssl']				= true;					// should remain false
+
+// Who receive all emails?
+$config['send_to']				= 'contacto@symmetry.la.test-google-a.com';			// destination of all emails sent throught contact form
+
+// Email Subject
+$config['subject']				= 'Contact Form';			// subject of emails you receive
+?>
